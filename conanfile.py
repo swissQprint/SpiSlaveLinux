26aa331c93daeb8d49ccecbea4154a5ab61ad2f9
Andreas Kalberer <aka@swissqprint.com>
2020-10-06 10:33:16 +0200

    Feature: Conan, CMake and pipeline files added for deployment of header-only library

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, CMake, python_requires
+
+build_tools = "sqpBuildTools/[~0.6]@sqp/release"
+sqp = python_requires(build_tools)
+
+class LibsqpSpiSlaveLinuxConan(ConanFile):
+    name = "libsqpSpiSlaveLinux"
+    version = sqp.get_version()
+    sqp.git_information()
+    license = "proprietary"
+    author = "swissQprint software@swissqprint.com"
+    url = "www.swissqprint.com"
+    description = "The libsqpSpiSlaveLinux includes the ioctl-definitions kernel- <-> userspace"
+    # settings = "arch"
+    # No settings/options are necessary, this is header only
+    generators = "cmake_paths"
+    build_requires = (build_tools)
+    exports_sources = "spi-slave-dev.h", "CMakeLists.txt", "cmake/sqpSpiSlaveLinuxConfig.cmake.in"
+    exports = "gitinfo.json"
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "conan_paths.cmake"
+        cmake.definitions.update(sqp.get_cmake_definitions(self.version))
+        cmake.configure()
+        return cmake
+
+    def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+
